bb88a8c1ebca45da8b0e4b7941e52fefa2f45078
tim-peterson <petersontimr@gmail.com>
2017-10-16 22:05:01 -0500

    functional code to write pumbed interactions to file

--- interacts_with_csv.py
+++ interacts_with_csv.py
@@ -4,29 +4,47 @@ abstracts=[]
 pmids=[]
 
 names = []
+ids = []
 with open('/Users/timrpeterson/Desktop/morpheome-10-16-17-aliases.csv','r') as f:
     next(f) # skip headings
     reader=csv.reader(f)
     for name in reader:
-    	names.append(name)
+    	names.append(name[1].lower())
+    	ids.append(name[3].lower())
 
+#print(names)
 pairs = []
 with open('/Users/timrpeterson/Desktop/morpheome-10-16-17-interacts_with.csv','r') as f:
     next(f) # skip headings
     reader=csv.reader(f)
-    for pmid,abstract in reader:
-    	words = abstract.partition("interacts with")
-    	before = words[0].split()[-1]
-    	#print("before" + before)
-
+    for abstract in reader:
+    	words = abstract[1].partition("physically interacts with")
+    	before = words[0].split()
     	after = words[2].split()
-    	if test_list.__contains('is') == 1:
-    		after = words[2].split()[0]
-    	if before in names:
-    		if after in names:
-    			pairs.append = [before,after]
+
+    	if len(before) >= 1:
+    		before = words[0].split()[-1].lower()
+    		#print("before" + before)
+
+	    	if len(after) >= 1:
+	    		after = words[2].split()[0].lower()
+	    		#print("after" + after)
+
+	    		#before = "ALIEN"
+		    	if before in names:
+		    		#after = "cbp"
+		    		before_i = names.index(before)
+		    		if after in names:
+		    			after_i = names.index(after)
+		    			pairs.append([before_i,after_i])
+
+		#break
+
+with open('pubmed-interactions-ids.csv','wb') as file:
+    for line in pairs:
+        file.write(str(line[0])+","+str(line[1]))
+        file.write('\n')
 
 
-
-print(pairs)
+#print(pairs)
 
